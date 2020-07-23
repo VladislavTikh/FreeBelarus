97aa148bd9d045331a43b13d67134541335914ff
Uladzislau Tsikhanovich <uladzislau_tsikhanovich@epam.com>
2020-07-23 14:59:49 +0300

    client side presentation compontents added for post entity

--- Client/Program.cs
+++ Client/Program.cs
@@ -19,7 +19,6 @@ namespace FreeBelarus.Client
             builder.RootComponents.Add<App>("app");
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSyncfusionBlazor();
-            builder.Services.AddBlazoredModal();
             await builder.Build().RunAsync();
         }
     }
