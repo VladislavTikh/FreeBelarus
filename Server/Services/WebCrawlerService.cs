f78db042ed7fc7f15a8b4e4668c6d20ae273133f
Uladzislau Tsikhanovich <tav_657@mail.ru>
2020-08-29 21:36:51 +0300

    ImoprtIO Client added

--- Server/Services/WebCrawlerService.cs
+++ Server/Services/WebCrawlerService.cs
@@ -1,29 +1,20 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace FreeBelarus.Server.Services
 {
     public class WebCrawlerService : IWebCrawlerService
     {
-        private HttpClient _client;
+        private ImportIOClient.ImportIOClient _client;
         private string _extractorId = "ca96e5a0-edd6-41fe-b1c0-a6b42b504b34";
-        private string _apiKey;
 
         public WebCrawlerService(string apiKey)
         {
-            _apiKey = apiKey;
-            _client = new HttpClient
-            {
-                BaseAddress = new Uri(@"https://data.import.io/extractor")
-            };
+            _client = new ImportIOClient.ImportIOClient(apiKey);
         }
 
         public async Task<string> GetJsonContentAsync()
         {
-            var uri = _client.BaseAddress + $"/{_extractorId}/json/latest?_apikey={_apiKey}";
-            var resposne = await _client.GetAsync(uri);
-            return await resposne.Content.ReadAsStringAsync();
+            return await _client.Data.GetLatestRunResultAsync(_extractorId);
         }
     }
 }
